e44e91ae4017cce818f175de49a329e8d217fcea
viji7237 <viji7237@gmail.com>
2017-02-26 19:59:53 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+﻿var express = require('express');
+var app = express();
+
+app.use('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log('Server started in port ', port);
+})
+
